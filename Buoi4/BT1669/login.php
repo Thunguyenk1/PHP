c3cff7d86a093e51ad770affc626e38978f06462
Thunguyenk1 <thunguyenhtay@gmail.com>
2024-01-12 21:04:58 +0700

    add readme into the project

---
+++ Buoi4/BT1669/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="card-header">
+            <h2>Trang Đăng Nhập</h2>
+        </div>
+        <div class="card-body">
+            <form action="" method="post">
+                <div class="form-group">
+                    <label for="">Email: </label>
+                    <input type="text" class="form-control" placeholder="Nhập email" name="email">
+                </div>
+                <div class="form-group">
+                    <label for="">Mật Khẩu: </label>
+                    <input type="text" class="form-control" placeholder="Nhập mật khẩu" name="password">
+                </div>
+                <br />
+                <div class="form-group">
+                    <label for=""></label>
+                    <button type="submit" class="btn btn-success">Đăng Ký</button>
+                </div>
+            </form>
+            
+        </div>
+    </div>
+</body>
+</html>
